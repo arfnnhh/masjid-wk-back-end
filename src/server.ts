685da3a325019d707f5da480da45899e364ee921
arfnnhh <arfanhashifhairuman@smkwikrama.sch.id>
2023-06-20 01:29:00 +0700

    nambahin mongoose lagi

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import app from "./app";
-
+import env from "./utility/validateEnv";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log("Server running on port " + port);
-});
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port " + port);
+    });
+  })
+  .catch(console.error);
