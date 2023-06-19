b96aa6a323f8487c6207d573e1f75bf78c372312
arfnnhh <arfanhashifhairuman@smkwikrama.sch.id>
2023-06-20 01:25:13 +0700

    changes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
 import app from "./app";
-import env from "./utility/validateEnv";
-import mongoose from "mongoose";
+
 
 const port = process.env.PORT || 5000;
 
-mongoose
-  .connect(env.MONGO_CONNECTION_STRING)
-  .then(() => {
-    console.log("Mongoose connected");
-    app.listen(port, () => {
-      console.log("Server running on port " + port);
-    });
-  })
-  .catch(console.error);
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
